026bf529df3f33ac443573e36cc7b80c87bd01e7
Paulo Pires <taralloqueiroiz@gmail.com>
2013-09-28 10:32:57 -0300

    começando app.js

---
+++ app/arrange/index.js
@@ -0,0 +1,27 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , path = require('path')
+  , chromelogger = require('chromelogger');
+
+var app = module.exports = express();
+
+// all environments
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('1a23sf5asdf2a1dfs2a3d4sf'));
+app.use(express.session());
+app.use(chromelogger.middleware);
+app.use(app.router);
+
+
+app.all('/', function (req, res)
+{	
+	res.redirect("/app/");
+});
