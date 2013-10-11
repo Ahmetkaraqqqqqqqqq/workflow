0022fb13605659ff2a4499f88ee1d2d5615629c9
Andre Felipe Carneiro <andrefelipecp@gmail.com>
2013-10-11 00:06:05 -0300

    configurando passport para logar com facebook

--- app/arrange/controllers/user.js
+++ app/arrange/controllers/user.js
@@ -1,13 +1,56 @@
 var models 	= app.get('arrange-models'),
-	_ 		= require("underscore");
+	_ 		= require("underscore"),
+	passport = require('passport'),
+    util = require('util'),
+    FacebookStrategy = require('passport-facebook').Strategy;
+
+var FACEBOOK_APP_ID = "--insert-facebook-app-id-here--"
+var FACEBOOK_APP_SECRET = "--insert-facebook-app-secret-here--";
 
 exports.add = function(params, callback){
-	console.log('parças', params)
 	models.User.create({
 		email : params.email,
-		username : params.username,
+		name : params.name,
 		password : params.password
 	}).success(function(user){
 		callback(user);
 	});
 }
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Facebook profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the FacebookStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Facebook
+//   profile), and invoke a callback with a user object.
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/#/users/login/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Facebook profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Facebook account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
