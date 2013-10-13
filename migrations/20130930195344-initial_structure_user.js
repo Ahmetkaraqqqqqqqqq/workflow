651017e2e09d3e53e4bdd0438f4f5e7d8723d035
Andre Felipe Carneiro <andrefelipecp@gmail.com>
2013-10-13 18:32:00 -0300

    adicionei arquivo de migracao do model user

---
+++ migrations/20130930195344-initial_structure_user.js
@@ -0,0 +1,20 @@
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    migration.createTable(
+    	'User', {
+			id:   			{ type: DataTypes.INTEGER, allowNull:false, primaryKey: true, autoIncrement: true},       // uid	int(11)	NO		NULL
+			name:  		  { type: DataTypes.STRING(255), allowNull: true},    //name	varchar(255)	NO		NULL	
+      email:       { type: DataTypes.STRING(255), allowNull: true},    //email  varchar(255)  NO    NULL  
+			password:	{ type: DataTypes.STRING(255), allowNull: false},    //pawword	varchar(255)	NO		NULL	
+			
+			created_at:     { type: DataTypes.DATE   , allowNull: false}, 	  //created_at	datetime	NO		NULL	
+			updated_at:     { type: DataTypes.DATE   , allowNull: false}  	  //updated_at	datetime	NO		NULL	
+    	}
+    );
+    done()
+  },
+  down: function(migration, DataTypes, done) {
+    migration.dropTable('User');
+    done()
+  }
+}
