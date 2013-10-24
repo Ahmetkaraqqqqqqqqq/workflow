5056ee008a5ae878e47523531730ce65915d6266
Andre Felipe Carneiro <andrefelipecp@gmail.com>
2013-10-24 00:39:56 -0200

    adicionei modulo nodemailer e email_template

--- app/admin/utils/mailerUtil.js
+++ app/admin/utils/mailerUtil.js
@@ -6,31 +6,20 @@ var Errors = require('./errors');
 // email templates
 var path = require('path');
 var emailTemplates = require('email-templates');
-var templatesDir = path.resolve(__dirname, '../views/', 'emailTemplates');
+var templatesDir = path.resolve(__dirname, '../admin/', 'emailTemplates');
 
 // configuration
 var env = process.env.NODE_ENV || 'development';
 var config = require('../../config/config')[env];
 // var logger = require('./loggerService.js').logger;
 
-// Create a transport object
-if (typeof(config.amazon) !== 'undefined') {
-  // Create an Amazon SES transport object
-  var transport = nodemailer.createTransport('SES', {
-    AWSAccessKeyID: config.amazon.AWSAccessKeyID,
-    AWSSecretKey: config.amazon.AWSSecretKey,
-    ServiceUrl: config.amazon.ServiceUrl
-  });
-}
-else {
-  var transport = nodemailer.createTransport('SMTP', {
-    service: config.smtp.service,
-    auth: {
-      user: config.smtp.user,
-      pass: config.smtp.pass
-    }
-  });
-}
+var transport = nodemailer.createTransport('SMTP', {
+  service: config.smtp.service,
+  auth: {
+    user: config.smtp.user,
+    pass: config.smtp.pass
+  }
+});
 
 /**
  * Send a single email
--- config/config.js
+++ config/config.js
@@ -15,4 +15,25 @@ config.db.user = process.env.RDS_USERNAME      || process.env.DB_USERNAME || def
 config.db.password =  process.env.RDS_PASSWORD || process.env.DB_PASSWORD || defaultConfig.password;
 config.db.dbname = process.env.RDS_DB_NAME     || process.env.DB_NAME     || defaultConfig.database;
 
+
+config.facebook: {
+        appId: "APP_ID"
+      , appSecret: "APP_SECRET"
+      , callbackURL: "http://localhost:3000/auth/facebook/callback"
+    };
+
+config.google: {
+        clientID: "APP_ID"
+      , clientSecret: "APP_SECRET"
+      , callbackURL: "http://localhost:3000/auth/google/callback"
+    };
+
+//EMAIL
+//for testing email, can leave as is or move to amazon SES / sendgrid
+config.smtp: { 
+        service: "Gmail" 
+      , user: "testeandrefelipecp"
+      , pass: "Teste2013"
+    }
+
 module.exports = config;
