a1dc3984694b7fac5b318b00449a968919d0917e
Andre Felipe Carneiro <andrefelipecp@gmail.com>
2013-10-24 00:43:40 -0200

    acertei problema na declaração dos arrays de configuracao

--- config/config.js
+++ config/config.js
@@ -16,13 +16,13 @@ config.db.password =  process.env.RDS_PASSWORD || process.env.DB_PASSWORD || def
 config.db.dbname = process.env.RDS_DB_NAME     || process.env.DB_NAME     || defaultConfig.database;
 
 
-config.facebook: {
+config.facebook = {
         appId: "APP_ID"
       , appSecret: "APP_SECRET"
       , callbackURL: "http://localhost:3000/auth/facebook/callback"
     };
 
-config.google: {
+config.google = {
         clientID: "APP_ID"
       , clientSecret: "APP_SECRET"
       , callbackURL: "http://localhost:3000/auth/google/callback"
@@ -30,7 +30,7 @@ config.google: {
 
 //EMAIL
 //for testing email, can leave as is or move to amazon SES / sendgrid
-config.smtp: { 
+config.smtp = { 
         service: "Gmail" 
       , user: "testeandrefelipecp"
       , pass: "Teste2013"
